0d04163598cbe5e8b47b18004e11a6d917d1e2dc
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-21 21:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let colorSelect = document.getElementById('colorSelect');
+let button = document.querySelector('input[type="button"]');
+
+function removeSelectedColor() {
+    let selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    colorSelect.removeChild(selectedOption);
+}
+
+button.addEventListener("click", removeSelectedColor);
